190731be58200fb084ff44ba30a184955e39dde6
THOSSYN <alextosin5@gmail.com>
2024-03-11 18:16:59 +0100

    Added utils/db.js

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    this.DB_HOST = process.env.DB_HOST || 'localhost';
+    this.DB_PORT = process.env.DB_PORT || 27017;
+    this.DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+    this.client = new MongoClient(`mongodb://${this.DB_HOST}:${this.DB_PORT}`);
+  }
+
+  async isAlive() {
+    try {
+      await this.client.connect();
+      return true;
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+      return false;
+    }
+  }
+
+  async nbUsers() {
+    const db = this.client.db(this.DB_DATABASE);
+    const userCount = await db.collection('users').countDocuments({});
+    return userCount;
+  }
+
+  async nbFiles() {
+    const db = this.client.db(this.DB_DATABASE);
+    const docCount = await db.collection('files').countDocuments({});
+    return docCount;
+  }
+}
+
+const dbClient = new DBClient();
+
+module.exports = dbClient;
